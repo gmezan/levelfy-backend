811ff29d2259774fa77c16d389fb5e45d8cb1fc7
Gustavo <gustavomeza27@gmail.com>
2021-02-22 17:39:16 +0000

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+docker stop levelfy-backend-0
+docker rm levelfy-backend-0
+git pull
 sudo mvn package
 docker build -t springio/levelfy-backend .
-docker run -d --restart unless-stopped -p 8080:8080 springio/levelfy-backend
+docker run -d --restart unless-stopped -p 8080:8080 --name levelfy-backend-0  springio/levelfy-backend
