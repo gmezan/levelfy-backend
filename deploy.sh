86ee227fb9d188929b6183631697cbf62d520680
Gustavo <gustavomeza27@gmail.com>
2021-03-07 18:04:36 +0000

    Correction

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ docker rm levelfy-backend-0
 git pull
 sudo mvn package
 docker build -t springio/levelfy-backend .
-docker run -d --restart unless-stopped -p 8080:8080 --name levelfy-backend-0  springio/levelfy-backend
+docker run -d --restart unless-stopped -p 8443:8443 --name levelfy-backend-0  springio/levelfy-backend
