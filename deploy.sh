952f9835bbf9e7ca84022288f39a367ba9a90469
root <root@ip-172-31-87-1.ec2.internal>
2021-03-15 03:42:34 +0000

    AAAAA

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 docker stop levelfy-backend-1
 docker rm levelfy-backend-1
 git pull
+sudo mvn clean
 sudo mvn package
+docker image rm springio/levelfy-backend
 docker build -t springio/levelfy-backend .
 docker run -d --restart unless-stopped -p 8080:8080 --name levelfy-backend-1  springio/levelfy-backend
