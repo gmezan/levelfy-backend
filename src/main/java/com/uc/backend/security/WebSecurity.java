2caab37e369ac6bbbf14818f8d7eb41af98478a0
gmn14 <meza.gustavo@pucp.edu.pe>
2021-03-19 19:56:43 -0500

    Security for Blog Post

--- src/main/java/com/uc/backend/security/WebSecurity.java
+++ src/main/java/com/uc/backend/security/WebSecurity.java
@@ -62,7 +62,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers("/model/course/list").permitAll() // To list available courses
                 .antMatchers("/model/service/form").permitAll() // to list ASES_PAQ, MAR
                 .antMatchers("/model/course-suggestion/open").permitAll()
-                .antMatchers("/blog/list").permitAll()
+                .antMatchers("/model/blog").permitAll()
 
                 .antMatchers("/model/user/me").authenticated()
                 .antMatchers("/model/user", "/model/user/*", "/model/user/**").hasAnyRole("ADMIN", "MOD")
