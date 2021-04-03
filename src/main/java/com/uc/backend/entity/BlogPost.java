150d988fda0b77270a2d55ecba1828f4d486d1ed
danu263 <montes.alex@pucp.edu.pe>
2021-04-03 11:12:02 -0500

    asd

--- src/main/java/com/uc/backend/entity/BlogPost.java
+++ src/main/java/com/uc/backend/entity/BlogPost.java
@@ -19,35 +19,59 @@ public class BlogPost extends Auditable implements Serializable {
         private String title;
         @Column(name="thumbnail",nullable = false)
         private String thumbnail;
+        @Column(name="description",nullable = false)
+        private String description;
         @Column(name="author",nullable = false)
         private String author;
         @Column(name="photo_author",nullable = false)
         private String photoAuthor;
-        @Column(name="body",nullable = false)
-        private String body;
+        @Column(name="badge",nullable = true)
+        private String badge;
         @Column(name="date_time",nullable = false)
         private LocalDateTime dateTime;
         @Column(name="time_read",nullable = false)
         private Integer timeRead;
+        @Column(name="fragments",nullable = false)
+        private String fragments;
 
+    public String getDescription() {
+        return description;
+    }
 
-        public int getId() { return id; }
-        public void setId(int value) { this.id = value; }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-        public String getTitle() { return title; }
-        public void setTitle(String value) { this.title = value; }
+    public String getBadge() {
+        return badge;
+    }
 
-        public String getThumbnail() { return thumbnail; }
-        public void setThumbnail(String value) { this.thumbnail = value; }
+    public void setBadge(String badge) {
+        this.badge = badge;
+    }
 
-        public String getAuthor() { return author; }
-        public void setAuthor(String value) { this.author = value; }
+    public String getFragments() {
+        return fragments;
+    }
 
-        public String getPhotoAuthor() { return photoAuthor; }
-        public void setPhotoAuthor(String value) { this.photoAuthor = value; }
+    public void setFragments(String fragments) {
+        this.fragments = fragments;
+    }
 
-        public String getBody() { return body; }
-        public void setBody(String value) { this.body = value; }
+    public int getId() { return id; }
+    public void setId(int value) { this.id = value; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String value) { this.title = value; }
+
+    public String getThumbnail() { return thumbnail; }
+    public void setThumbnail(String value) { this.thumbnail = value; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String value) { this.author = value; }
+
+    public String getPhotoAuthor() { return photoAuthor; }
+    public void setPhotoAuthor(String value) { this.photoAuthor = value; }
 
 
     public LocalDateTime getDateTime() {
