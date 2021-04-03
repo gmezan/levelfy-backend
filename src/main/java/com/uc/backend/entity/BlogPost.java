1ddce45b627e1ac0b8e4bfa4608ef16800fa4d9b
danu263 <montes.alex@pucp.edu.pe>
2021-04-03 11:14:13 -0500

    Asdasqwe

--- src/main/java/com/uc/backend/entity/BlogPost.java
+++ src/main/java/com/uc/backend/entity/BlogPost.java
@@ -10,29 +10,29 @@ import java.time.*;
 @Entity
 @Table(name="blog_post")
 public class BlogPost extends Auditable implements Serializable {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name="idblog_post",nullable = false)
-        private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idblog_post",nullable = false)
+    private int id;
 
-        @Column(name="title",nullable = false)
-        private String title;
-        @Column(name="thumbnail",nullable = false)
-        private String thumbnail;
-        @Column(name="description",nullable = false)
-        private String description;
-        @Column(name="author",nullable = false)
-        private String author;
-        @Column(name="photo_author",nullable = false)
-        private String photoAuthor;
-        @Column(name="badge",nullable = true)
-        private String badge;
-        @Column(name="date_time",nullable = false)
-        private LocalDateTime dateTime;
-        @Column(name="time_read",nullable = false)
-        private Integer timeRead;
-        @Column(name="fragments",nullable = false)
-        private String fragments;
+    @Column(name="title",nullable = false)
+    private String title;
+    @Column(name="thumbnail",nullable = false)
+    private String thumbnail;
+    @Column(name="description",nullable = false)
+    private String description;
+    @Column(name="author",nullable = false)
+    private String author;
+    @Column(name="photo_author",nullable = false)
+    private String photoAuthor;
+    @Column(name="badge",nullable = true)
+    private String badge;
+    @Column(name="date_time",nullable = false)
+    private LocalDateTime dateTime;
+    @Column(name="time_read",nullable = false)
+    private Integer timeRead;
+    @Column(name="fragments",nullable = false)
+    private String fragments;
 
     public String getDescription() {
         return description;
