0f9e3a7282fec7d8e6fda7d14ed9e44e74530817
gmn14 <meza.gustavo@pucp.edu.pe>
2021-04-03 13:25:52 -0500

    Refactorizing AWS S3 API

--- src/main/java/com/uc/backend/config/AmazonConfig.java
+++ src/main/java/com/uc/backend/config/AmazonConfig.java
@@ -5,18 +5,26 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class AmazonConfig {
 
-    @Value("${awscredential.accesskey}")
-    public static String accessKey;
+    private Environment env;
 
-    @Value("${awscredential.secretkey}")
-    public static String secretKey;
+    @Autowired
+    public AmazonConfig(Environment env) {
+        this.env = env;
+        this.accessKey = this.env.getProperty("awscredential.accesskey");
+        this.secretKey = this.env.getProperty("awscredential.secretkey");
+    }
+
+    private final String accessKey;
+    private final String secretKey;
 
     @Bean
     public AmazonS3 s3() {
